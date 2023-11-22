349f4e55a0b0817fb6d140e0385346ff196c0035
Frnn4268 <appstutorialesy@gmail.com>
2023-11-21 23:25:33 -0600

    Uploading new changes to this exercise :p

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,42 +7,51 @@ const Header = (props) => { /* Módulo más detallado */
 }
 
 const Part = (props) => {
-  return <p>{props.part}, {props.exercises}</p>
+  return <p>{props.part.name}, {props.part.exercises}</p>
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
   ) 
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.total + props.total + props.total}</p>
+  return <p>Number of exercises {props.total}</p>
 }
 
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content 
-        part1={part1} exercises1={exercises1} 
-        part2={part2} exercises2={exercises2} 
-        part3={part3} exercises3={exercises3} 
+        part1={course.parts[0]} 
+        part2={course.parts[1]}  
+        part3={course.parts[2]} 
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
