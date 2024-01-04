6d83961aa9437c7e6f99821d3994cce6d0b26eb3
Frnn4268 <appstutorialesy@gmail.com>
2024-01-03 19:06:58 -0600

    Adding comments in the code and using a post to create a new user in the test database

--- part3/cypress/e2e/note_app.cy.js
+++ part3/cypress/e2e/note_app.cy.js
@@ -1,16 +1,31 @@
+// This file tests the Note App.
 describe('Note App', () => {
+    // Before each test, reset the database and create a new user.
     beforeEach(() => {
         cy.visit('http://localhost:3000')
+
+        cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        
+        const user = {
+            name: 'Fernando',
+            username: 'Frnn',
+            password: '12345'
+        }
+
+        cy.request('POST', 'http://localhost:3001/api/users', user)
     }) 
 
+    // Test that the frontpage can be opened.
     it('frontpage can be opened', () => { 
         cy.contains('Notes')
     })
 
+    // Test that the login form can be opened.
     it('login form can be opened', () => {
         cy.contains('Show login').click()
     })
 
+    // Test that a user can write in the inputs and login.
     it('user can write in the inputs and login', () => {
         cy.contains('Show login').click()
         cy.get('[placeholder="Username"]').type('Frnn')
@@ -19,7 +34,9 @@ describe('Note App', () => {
         cy.contains('New note')
     })
 
+    // Test the functionality of the Note App when a user is logged in.
     describe('when user logged in', () => {
+        // Before each test, log in the user.
         beforeEach(() => {
             cy.contains('Show login').click()
             cy.get('[placeholder="Username"]').type('Frnn')
@@ -28,6 +45,7 @@ describe('Note App', () => {
             cy.contains('New note')
         })
 
+        // Test that a new note can be created.
         it('a new note can be created', () => {
             const noteContent = 'a note created by cypress'
             cy.contains('New note').click()
